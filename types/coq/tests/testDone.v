27306e7f8530ec0e145fa09aae2fc75fce7806a6
Jeremy Buisson <jeremy.buisson@irisa.fr>
2014-12-03 18:05:10 +0100

    new type proof for the Done test, as generated by the Coq generator

--- types/coq/tests/testDone.v
+++ types/coq/tests/testDone.v
@@ -1,35 +1,20 @@
-Require Import List.
+Require Import SosADL.
 Require Import String.
-Require Import AbstractSoSADL.
+Require Import List.
+Require Import BinInt.
 
-Import AST.
 Import ListNotations.
 
+Local Open Scope list_scope.
+Local Open Scope string_scope.
+Local Open Scope Z_scope.
+
 Definition ast :=
-  SosADL
-    []
-    (Library
-       "testDone"
-       (EntityBlock
-          []
-          []
-          [SystemDecl
-              "test"
-              []
-              [DataTypeDecl "type1" None []]
-              []
-              (BehaviorDecl
-                 "test"
-                 []
-                 (Behavior [Done]))
-              None]
-          []
-          [])).
+(SosADL [] (Some (Library (Some "testDone") (Some (EntityBlock [] [] [(SystemDecl (Some "test") [] [(DataTypeDecl (Some "type1") None [])] [(GateDecl (Some "gate1") [(Connection (Some false) (Some "connection1") (Some ModeTypeIn) (Some (NamedType (Some "type1"))))] (Some (ProtocolDecl (Some "gate1protocol") (Some (Protocol [ProtocolStatement_Done])))))] (Some (BehaviorDecl (Some "test") (Some (Behavior [BehaviorStatement_Done])))) None)] [] []))))).
 
 (*
 Local Variables:
 mode: coq
 coding: utf-8
-coq-load-path: ("..")
 End:
  *)
--- types/coq/tests/testDone_proof.v
+++ types/coq/tests/testDone_proof.v
@@ -2,7 +2,12 @@ Require Import TypeSystem.
 Require Import testDone.
 
 Import List.
-Import AbstractSoSADL.AST.
+Import AST.
+Import ListNotations.
+
+Local Open Scope string_scope.
+Local Open Scope list_scope.
+Local Open Scope Z_scope.
 
 Theorem well_typed: SoSADL ast well typed.
 Proof.
@@ -10,18 +15,27 @@ Proof.
   (* repeat (try constructor; try split). *)
   apply type_SosADL.
   apply type_Library.
-  apply type_EntityBlock_system; split.
+  apply type_EntityBlock_system with "test"; split.
   - apply type_SystemDecl; split.
     + apply Forall_nil.
-    + apply type_SystemDecl_datatype; split.
+    + apply type_SystemDecl_datatype with "type1"; repeat split.
       * apply type_DataTypeDecl_None.
-      * apply type_SystemDecl_None.
-        {
-          apply type_BehaviorDecl; split.
-          - apply Forall_nil.
-          - apply type_Done.
-        }
-  - apply type_EntityBlock.
+      * { apply type_SystemDecl_gate with "gate1"; repeat split.
+          - admit.
+          - simpl. apply NoDup_cons.
+            + auto.
+            + apply NoDup_nil.
+          - apply Forall_cons.
+            + apply type_Connection.
+              * { apply type_NamedType with (DataTypeDecl (Some "type1") None []).
+                  - admit. }
+            + apply Forall_nil.
+          - apply type_SystemDecl_None.
+            + apply type_BehaviorDecl.
+              * apply type_Done. }
+  - split.
+    + reflexivity.
+    + apply type_EntityBlock.
 Qed.
 
 (*
