0f07f5bf0df9da32c11927be056769b587fbc6c0
Jeremy Buisson <jeremy.buisson@irisa.fr>
2015-03-12 08:58:37 +0100

    typing proofs of the latest testChoose samples

---
+++ types/coq/tests/testChoose7_proof.v
@@ -0,0 +1,42 @@
+Require Import TypeSystem.
+Require Import tests.testChoose7.
+
+Import List.
+Import AST.
+Import ListNotations.
+Import Utils.
+Import String.
+Import Interpretation.
+
+Local Open Scope string_scope.
+Local Open Scope list_scope.
+Local Open Scope Z_scope.
+
+Definition type1: t_DataType := RangeType (Some (IntegerValue (Some 1)))
+                                         (Some (IntegerValue (Some 2))).
+
+Theorem well_typed: ~ SoSADL ast well typed.
+Proof.
+  unfold ast. intro WT.
+  inversion_clear WT.
+  inversion_clear H.
+  inversion_clear H0.
+  decompose [and] H. clear H H2 H3.
+  inversion_clear H0.
+  decompose [and] H. clear H H0.
+  inversion_clear H1.
+  decompose [and] H. clear H H0.
+  inversion_clear H1.
+  decompose [and] H. clear H H0 H2.
+  inversion_clear H3.
+  inversion_clear H.
+  inversion_clear H0.
+Qed.
+
+(*
+Local Variables:
+mode: coq
+coding: utf-8
+coq-load-path: ("..")
+End:
+ *)
