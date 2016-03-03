d47e4ace1af0fa7e2571f028fb07ee81eb93589c
Jeremy Buisson <jeremy.buisson@irisa.fr>
2016-03-03 13:38:57 +0100

    updating some scripts

--- types/coq/make-allpdf.sh
+++ types/coq/make-allpdf.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-rm all.pdf
-make -j2 && make 'COQDOCLIBS=-p "\usepackage{listings}"' all.pdf
+rm -f all.pdf
+make -j2 &&
+    find SosADL -name '*.v' | xargs coqdep -c -sort -suffix .v | xargs coqdoc -toc -interpolate -utf8 -latex -p "\usepackage{listings}" -o all.tex &&
+    latexmk -pdf all.tex
+
