91f255cf1d6c5c49092f67c3ff84f9776486b1e6
eduardo <eduafsilva@gmail.com>
2018-07-10 09:41:36 -0300

    New project: PlasmaLab connector

---
+++ org.archware.sosadl.plasmalab/src/org/archware/sosadl/plasmalab/events/EventConverter.java
@@ -0,0 +1,21 @@
+package org.archware.sosadl.plasmalab.events;
+
+import java.util.List;
+
+import org.archware.sosadl.execution.events.ExecutionEvent;
+
+import events.Event;
+
+/**
+ * This class converts simulator's events to PlasmaLab events
+ * @author Eduardo
+ *
+ */
+public class EventConverter {
+
+	public static List<Event> convertAll(List<ExecutionEvent> events) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
